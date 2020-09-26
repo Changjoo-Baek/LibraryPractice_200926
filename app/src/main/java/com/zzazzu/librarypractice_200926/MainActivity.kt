2bb2105750ed865fc139338d281045051caa452c
bucephalus41 <bucephalus41@gmail.com>
2020-09-26 15:49:47 +0900

    일단 다 따라함

--- app/src/main/java/com/zzazzu/librarypractice_200926/MainActivity.kt
+++ app/src/main/java/com/zzazzu/librarypractice_200926/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.zzazzu.librarypractice_200926
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import com.bumptech.glide.Glide
+import com.gun0912.tedpermission.PermissionListener
+import com.gun0912.tedpermission.TedPermission
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.jar.Manifest
 
 class MainActivity : BaseActivity() {
 
@@ -19,9 +23,31 @@ class MainActivity : BaseActivity() {
 
     override fun setupEvents() {
 
-    }
+        callPhoneBtn.setOnClickListener{
 
-    override fun setValues() {
+            val    pl = object : PermissionListener {
+                override fun onPermissionGranted() {
+
+                    val myUri = Uri.parse("tel:${phoneNumID}")
+                    val myIntent = Intent(Intent.ACTION_CALL, myUri)
+                    startActivity(myIntent)
+                }
+
+                override fun onPermissionDenied(deniedPermissions: MutableList<String>?) {
+                    Toast.makeText(mContext, "권한이 거부되어 통화가 안 됩니다.", Toast.LENGTH_SHORT).show()
+                }
+
+            }
+
+// [설정]으로 입력해두면 나중에 메시지에서 설정을 클릭했을 때 설정 메뉴로 이동이 가능하다.
+
+            TedPermission.with(mContext)
+                .setPermissionListener(pl)
+                .setDeniedMessage("권한이 거부되었습니다. [설정] 에서 권한을 켜주세요")
+                .setPermissions(Manifest.permission.CALL_PHONE)
+                .check()
+        }
+
 
         loadWebImageButton.setOnClickListener{
 
@@ -36,6 +62,12 @@ class MainActivity : BaseActivity() {
             val myIntent = Intent(mContext, ViewPrototypeActivity :: class.java)
             startActivity(myIntent)
         }
+
+    }
+
+    override fun setValues() {
+
+
 
     }
 
