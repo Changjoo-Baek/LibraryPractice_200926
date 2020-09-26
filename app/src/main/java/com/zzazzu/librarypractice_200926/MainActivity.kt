87d4020e29c1c1bc8d80b2e876409d09665f3858
bucephalus41 <bucephalus41@gmail.com>
2020-09-26 14:06:50 +0900

    맞게 따라하고 있는건가,,
    인터넷 접속 권한 득했고, 이미지 링크까지 넣었는데 테스트를 못해봄..

--- app/src/main/java/com/zzazzu/librarypractice_200926/MainActivity.kt
+++ app/src/main/java/com/zzazzu/librarypractice_200926/MainActivity.kt
@@ -1,11 +1,42 @@
 package com.zzazzu.librarypractice_200926
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupEvents()
+        setValues()
+
     }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+        loadWebImageButton.setOnClickListener{
+
+            Glide.with(mContext).load("https://nsimg.kbs.co.kr/data/news/2019/12/08/4338865_Bjk.jpg).into(otherPersonIm").into(otherPersonImg)
+        }
+
+
+        profilePhotoImg.setOnClickListener {
+            Toast.makeText(mContext, "사진 클릭됨", Toast.LENGTH_SHORT).show()
+
+
+            val myIntent = Intent(mContext, ViewPrototypeActivity :: class.java)
+            startActivity(myIntent)
+        }
+
+    }
+
 }
---
+++ app/src/main/java/com/zzazzu/librarypractice_200926/ViewPrototypeActivity.kt
@@ -0,0 +1,24 @@
+package com.zzazzu.librarypractice_200926
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ViewPrototypeActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_prototype)
+        setupEvents()
+        setValues()
+
+
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+
+}
